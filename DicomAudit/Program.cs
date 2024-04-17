d13f8f1d5d5d7e95e4f73d0c523e458add25f517
James A Sutherland <j@sutherland.pw>
2024-04-17 13:59:34 -0500

    Initial implementation

---
+++ DicomAudit/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FellowOakDicom;
+using LibArchive.Net;
+
+long objectCount = 0;
+
+Parallel.ForEach(Directory.EnumerateFiles(".", "*", SearchOption.AllDirectories), ProcessFile);
+return;
+
+bool ProcessStream(Stream s, string path)
+{
+    Span<byte> preamble = stackalloc byte[4];
+    s.Seek(128, SeekOrigin.Begin);
+    if (s.Read(preamble) != 4 || "DICM"u8 != preamble) return false;
+
+    s.Seek(0, SeekOrigin.Begin);
+    var ds = DicomFile.Open(s).Dataset;
+    Console.WriteLine($"{ds.GetString(DicomTag.Modality)},{ds.GetString(DicomTag.StudyInstanceUID)},{ds.GetString(DicomTag.SeriesInstanceUID)},{ds.GetString(DicomTag.SOPInstanceUID)},{path}");
+    var counter = Interlocked.Increment(ref objectCount);
+    if (counter % 1024 == 0) Console.Error.Write($"{counter}\r");
+    return true;
+}
+
+void ProcessArchive(string path)
+{
+    using var arc = new LibArchiveReader(path);
+    foreach (var entry in arc.Entries())
+    {
+        using var s = entry.Stream;
+        ProcessStream(s, $"{path}!{entry.Name}");
+    }
+}
+
+void ProcessFile(string path)
+{
+    var s = File.OpenRead(path);
+    if (s.Length < 132 || ProcessStream(s, path)) return;
+
+    ProcessArchive(path);
+}
